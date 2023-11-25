cc38e85f0efa6d9623fad846e391b5de1e129bb7
ryanjeffares <ryandjeffares@gmail.com>
2023-11-25 14:42:42 +0000

    split consumption into categories in countries

--- csv-transform/src/main.cpp
+++ csv-transform/src/main.cpp
@@ -160,26 +160,18 @@ int main(int argc, const char* argv[])
     const auto consumption = std::stod(row[1]);
     const auto emissions = std::stod(row[2]);
 
-    if (!countryData.contains("total_consumption")) {
-      countryData["total_consumption"] = 0;
+    if (!countryData.contains("Total")) {
+      countryData["Total"]["consumption"] = 0;
+      countryData["Total"]["emissions"] = 0;
     }
 
-    countryData["total_consumption"] = countryData["total_consumption"].get<double>() + consumption;
-
-    if (!countryData.contains("total_emissions")) {
-      countryData["total_emissions"] = 0;
-    }
-
-    countryData["total_emissions"] = countryData["total_emissions"].get<double>() + emissions;
+    countryData["Total"]["consumption"] = countryData["Total"]["consumption"].get<double>() + consumption;
+    countryData["Total"]["emissions"] = countryData["Total"]["emissions"].get<double>() + consumption;
 
     for (const auto& category : s_categoryNames) {
       if (category == categoryName) {
-        const auto consumptionKeyName = category + "_consumption";
-        const auto emissionsKeyName = category + "_emissions";
-
-        countryData[consumptionKeyName] = consumption;
-        countryData[emissionsKeyName] = emissions;
-
+        countryData[category]["consumption"] = consumption;
+        countryData[category]["emissions"] = emissions;
         break;
       }
     }
@@ -200,8 +192,8 @@ int main(int argc, const char* argv[])
     auto data = nlohmann::json();
     auto totalConsumption = 0.0, totalEmissions = 0.0;
     for (const auto& country : finalJson["countries"]) {
-      const auto consumption = country[category + "_consumption"].get<double>();
-      const auto emissions = country[category + "_emissions"].get<double>();
+      const auto consumption = country[category]["consumption"].get<double>();
+      const auto emissions = country[category]["emissions"].get<double>();
       totalConsumption += consumption;
       totalEmissions += emissions;
     }
